e6343710b543c7634bc8b722f026effc566952b3
solivella <olivella@princeton.edu>
2016-12-06 11:14:43 -0500

    Added quantile function

--- dev/test/test_poisbinom.R
+++ dev/test/test_poisbinom.R
@@ -8,18 +8,18 @@ phis <- runif(5000)
 eval.vec <- 0:5000
 
 #Test dpoisbinom
-# cat("Test probability mass function:\n")
-# identical(round(dpoibin(eval.vec,phis), 6), round(dpoisbinom(eval.vec,phis, FALSE), 6))
-# microbenchmark(dpoibin(eval.vec,phis)
-#               ,dpoisbinom(eval.vec, phis, FALSE),
-#                times = 5L)
-# 
-# #Test ppoisbinom
-# cat("Test cumulative distribution function:\n")
-# identical(round(ppoibin(eval.vec,phis), 6), round(ppoisbinom(eval.vec,phis, TRUE, FALSE), 6))
-# microbenchmark(ppoibin(eval.vec,phis)
-#                ,ppoisbinom(eval.vec, phis, TRUE, FALSE),
-#                times = 5L)
+cat("Test probability mass function:\n")
+identical(round(dpoibin(eval.vec,phis), 6), round(dpoisbinom(eval.vec,phis, FALSE), 6))
+microbenchmark(dpoibin(eval.vec,phis)
+              ,dpoisbinom(eval.vec, phis, FALSE),
+               times = 5L)
+
+#Test ppoisbinom
+cat("Test cumulative distribution function:\n")
+identical(round(ppoibin(eval.vec,phis), 6), round(ppoisbinom(eval.vec,phis, TRUE, FALSE), 6))
+microbenchmark(ppoibin(eval.vec,phis)
+               ,ppoisbinom(eval.vec, phis, TRUE, FALSE),
+               times = 5L)
 
 #Test qpoisbinom
 cat("Test quantile function:\n")
